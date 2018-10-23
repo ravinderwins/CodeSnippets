23a5cee4981638c8427b48e271ba9286895333c7
Saddam Husaain <saddamhusaainwins@gmail.com>
2018-10-23 11:12:10 +0530

    Added code sample of converting .doc to .docx document.

---
+++ C#/ConvertDocToDocx/ConvertDocToDocx/Program.cs
@@ -0,0 +1,23 @@
+﻿using ConvertDocToDocx.Sample;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConvertDocToDocx
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Utility merge = new Utility(ConfigurationSettings.AppSettings["SourcePath"], ConfigurationSettings.AppSettings["TargetPath"]);
+            merge.ConvertDocument();
+
+
+            Console.WriteLine("\n\nPress any key to exit.");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ C#/ConvertDocToDocx/ConvertDocToDocx/Sample/Utility.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Word = Microsoft.Office.Interop.Word;
+
+
+namespace ConvertDocToDocx.Sample
+{
+    public class Utility
+    {
+        string _sourcePath, _targetpath;
+
+        public Utility(string SourcePath, string TargetPath)
+        {
+            _sourcePath = SourcePath;
+            _targetpath = TargetPath;
+        }
+        public void ConvertDocument()
+        {
+            Word._Application application = new Word.Application();
+            object fileformat = Word.WdSaveFormat.wdFormatXMLDocument;
+            DirectoryInfo directory = new DirectoryInfo(@_sourcePath);
+            foreach (FileInfo file in directory.GetFiles("*.doc", SearchOption.AllDirectories))
+            {
+                if (file.Extension.ToLower() == ".doc")
+                {
+                    object filename = file.FullName;
+                    string newfilename = file.Name.Replace(".doc", ".docx");
+                    Word._Document document = application.Documents.Open(filename);
+
+                    document.Convert();
+                    document.SaveAs2(_targetpath +'/'+ newfilename, fileformat);
+                    document.Close();
+                    document = null;
+                }
+            }
+            application.Quit();
+            application = null;
+        }
+    }
+}
