1280c950237a95c8a10c88e8f10929cf2c17826a
Saddam Husaain <saddamhusaainwins@gmail.com>
2018-10-23 11:31:17 +0530

    Added sample project for replacing bookmark using open document liberary of Microsoft.

---
+++ C#/WordMergeUtility/WordMergeUtility/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WordMergeUtility
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DocumentMerge merge = new DocumentMerge(ConfigurationSettings.AppSettings["SourcePath"], ConfigurationSettings.AppSettings["TargetPath"]);
+            merge.MergeDocument();
+
+
+            Console.WriteLine("\n\nPress any key to exit.");
+            Console.ReadKey();
+        }
+    }
+}
